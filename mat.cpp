ffc4ca03750cf8a81896a5a5bbd4fc79a90ef5c8
Anthony Assayah <92322613+AnthonyAssayah@users.noreply.github.com>
2022-03-08 21:27:32 +0200

    Add files via upload

---
+++ mat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace ariel{
+    string mat(int rows, int cols, char c1, char c2){
+
+        return "";
+    }
+}
---
+++ mat.hpp
@@ -0,0 +1,15 @@
+// #ifndef MAT_INCLUDE
+// #define MAT_INCLUDE
+
+
+#include <string>
+using namespace std;
+
+
+namespace ariel
+{
+    string mat(int x, int y, char a, char b);
+} // namespace ariel
+
+
+// #endif
